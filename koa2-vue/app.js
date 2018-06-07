eac423ebe3cce6c06639d696b6a35bc04cf74bc7
yinxs <xuesong_yin@126.com>
2018-06-07 17:20:06 +0800

    日志

--- koa2-vue/app.js
+++ koa2-vue/app.js
@@ -18,19 +18,43 @@ app.use(bodyparser({
 }))
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(require('koa-static')(__dirname + '/public/fronted/dist'))
 
 app.use(views(__dirname + '/views', {
   extension: 'pug'
 }))
 
+//log工具
+const logUtil = require('./public/utils/log_util');
+
+
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+    //响应开始时间
+    const start = new Date();
+    //响应间隔时间
+    let ms;
+    try {
+        //开始进入到下一个中间件
+        await next();
+
+        ms = new Date() - start;
+        //记录响应日志
+        logUtil.logResponse(ctx, ms);
+
+    } catch (error) {
+        ctx.body={
+            data:null,
+            message:error,
+            status:101,
+            success:false
+        };
+
+        ms = new Date() - start;
+        //记录异常日志
+        logUtil.logError(ctx, error, ms);
+    }
+});
 
 // routes
 app.use(index.routes(), index.allowedMethods())
@@ -38,7 +62,9 @@ app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+    let ms = new Date() - start;
+    //一层保险
+    logUtil.logError(ctx, err, ms);
 });
 
 module.exports = app
